a688000c71f22d5c73b82b79f77044fd130b4d7c
A veagI <igaevbilich@mail.ru>
2021-02-16 18:59:43 +0400

    Added Product logical in product_page.py, test_product_page.py, locators.py

--- pages/locators.py
+++ pages/locators.py
@@ -9,3 +9,16 @@ class LoginPageLocators():
     LOGIN_FORM = (By.CSS_SELECTOR, "#login_form")
     REGISTER_FORM = (By.CSS_SELECTOR, "#register_form")
     LOGIN_URL = 'login'
+
+
+class ProductPageLocators():
+    # определяем кортежи для нахождения кнопки добавить в корзину, имени продукта, цены продукта, сообщения об
+    # успешном добавлении продукта в корзину, сообщения о соответствии цене корзины, имени продукта в сообщении об
+    # успешном добавлении продукта в корзину, общей стоимости корзины после добавления продукта
+    BUTTON_ADD_TO_BASKET = (By.CSS_SELECTOR, ".btn.btn-lg.btn-primary.btn-add-to-basket")
+    PRODUCT_NAME = (By.CSS_SELECTOR, ".col-sm-6.product_main h1")
+    PRODUCT_PRICE = (By.CSS_SELECTOR, ".col-sm-6.product_main .price_color")
+    MESSAGE_SUCCESS_ADDED_TO_BASKET = (By.CSS_SELECTOR, "div.alert-success.fade.in:nth-child(1) div")
+    MESSAGE_PRICE_BASKET = (By.CSS_SELECTOR, "div.alert-info.fade.in p:nth-child(1)")
+    PRODUCT_NAME_IN_MESSAGE_SUCCESS_ADDED_TO_BASKET = (By.CSS_SELECTOR, "div.alert-success.fade.in:nth-child(1) strong")
+    PRODUCT_PRICE_IN_MESSAGE_PRICE_BASKET = (By.CSS_SELECTOR, "div.alert-info.fade.in strong")
---
+++ pages/product_page.py
@@ -0,0 +1,34 @@
+import time
+from .base_page import BasePage
+from .locators import ProductPageLocators
+
+
+class ProductPage(BasePage):
+    def add_to_basket(self):
+        # находим и нажимаем на кнопку добавить в корзину
+        add_to_basket = self.browser.find_element(*ProductPageLocators.BUTTON_ADD_TO_BASKET)
+        add_to_basket.click()
+        # проходим промт и алерт
+        self.solve_quiz_and_get_code()
+
+    def should_be_massage_product_added_to_basket(self):
+        # сохраням переменные имени продукта и имени продукта в сообщения об успешном добавлении продуктав корзину
+        product_name = self.browser.find_element(*ProductPageLocators.PRODUCT_NAME).text
+        product_name_in_message_add = self.browser.find_element(*ProductPageLocators.PRODUCT_NAME_IN_MESSAGE_SUCCESS_ADDED_TO_BASKET).text
+        # проверяем наличие сообщения об успешном добавлении товара в корзину
+        assert self.is_element_present(*ProductPageLocators.MESSAGE_SUCCESS_ADDED_TO_BASKET), f"There is no message " \
+                                                                                              f"about successful " \
+                                                                                              f"addition {product_name}to basket "
+        # проверяем равенство имени продукта и имени продукта в сообщении об успешном доавлении в корзину
+        assert product_name == product_name_in_message_add, f"{product_name} != {product_name_in_message_add}"
+
+    def should_be_message_price_basket(self):
+        # сохраням переменные имени продукта, цены продукта и общей стоимости корзины
+        product_name = self.browser.find_element(*ProductPageLocators.PRODUCT_NAME).text
+        product_price = self.browser.find_element(*ProductPageLocators.PRODUCT_PRICE).text
+        product_price_in_message_price = self.browser.find_element(*ProductPageLocators.PRODUCT_PRICE_IN_MESSAGE_PRICE_BASKET).text
+        # проверяем наличие сообщения о цене корзины
+        assert self.is_element_present(*ProductPageLocators.MESSAGE_PRICE_BASKET), f"There is no message about the " \
+                                                                                   f"price of the {product_name.text} in the basket"
+        # проверяем оавенство цены продукта и общей стоимости корзины
+        assert product_price == product_price_in_message_price, f"{product_name} != {product_price_in_message_price}"
